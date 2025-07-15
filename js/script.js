c82981b0e29820042370305f31269c91b9ce2822
Anamul Haque Anam <anamkhanbd2@gmail.com>
2025-07-15 18:51:05 +0600

    Add documentation comment for switch statement to identify the current day

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
+/**
+ * make a switch statement to know which day is today !!!
+ * 
+ */
+
 switch (new Date().getDay()) {
     case 0:
         console.log("Sunday");
         break;
-        
+
         case 1:
         console.log("Monday");
         break;
